a36197735dc7f96f0f17bdfa0bddf7a8be74bec0
exellentcoin26 <jonas.caluwe26@gmail.com>
2023-06-11 17:06:11 +0200

    Document code and fix token positions

--- src/regex.rs
+++ src/regex.rs
@@ -1,23 +1,34 @@
 #![allow(dead_code)]
 #![allow(unused)]
 
-use std::{iter::Peekable, str::Chars};
+use std::{
+    iter::{Enumerate, Peekable},
+    str::Chars,
+};
 
 pub struct Tokenizer<'a> {
+    /// Input the tokenizer will tokenize.
     input: &'a str,
-    cursor_pos: usize,
-    it: Peekable<Chars<'a>>,
+    /// Iterator over the characters in the input (as defined in the rust `char`
+    /// type), along with their position in the input.
+    it: Peekable<Enumerate<Chars<'a>>>,
+    /// Temporary list of tokens from the tokenized input.
+    /// TODO: Convert to iterator.
     tokens: Vec<Token>,
 }
 
 #[derive(Debug)]
 pub struct Token {
+    /// Information about the kind of token along with the value of the token.
+    /// The value can already be parsed (e.g. unicode escape sequences)
     kind: TokenKind,
-    pos: usize,
+    /// Start and end position of the token in the input text. The end position
+    /// is one further than the end of the current token.
+    pos: (usize, usize),
 }
 
 impl Token {
-    pub fn new(kind: TokenKind, pos: usize) -> Self {
+    pub fn new(kind: TokenKind, pos: (usize, usize)) -> Self {
         Self { kind, pos }
     }
 }
@@ -56,32 +67,35 @@ impl<'a> Tokenizer<'a> {
     pub fn new(input: &'a str) -> Self {
         Self {
             input,
-            cursor_pos: 0,
-            it: input.chars().peekable(),
+            it: input.chars().enumerate().peekable(),
             tokens: Vec::new(),
         }
     }
 
     pub fn tokenize(mut self) -> Vec<Token> {
-        while let Some(ch) = self.it.next() {
+        while let Some((start_cursor_pos, ch)) = self.it.next() {
             match ch {
                 '\\' => {
                     let token_kind = self.take_class_or_escape_character();
-                    self.tokens.push(Token::new(token_kind, self.cursor_pos));
+                    let end_cursor_pos = match self.it.peek() {
+                        Some((end_cursor_pos, _)) => *end_cursor_pos,
+                        None => start_cursor_pos,
+                    };
+                    self.tokens
+                        .push(Token::new(token_kind, (start_cursor_pos, end_cursor_pos)));
                 }
 
                 _ => unimplemented!("character not handled! ({})", ch),
             }
-
-            self.cursor_pos += 1;
         }
 
         self.tokens
     }
 
     fn take_class_or_escape_character(&mut self) -> TokenKind {
-        let Some(ch) = self.it.next() else {
-            /// The last token of the input was a '\', which should always be pared. 
+        let Some((_, ch)) = self.it.next() else {
+            // The last token of the input was a '\', which should always be
+            // pared.
             return TokenKind::Invalid;
         };
 
@@ -110,23 +124,28 @@ impl<'a> Tokenizer<'a> {
             'D' => ClassKind::NonDigit,
             's' => ClassKind::Whitespace,
             'S' => ClassKind::NonWhitespace,
+
             _ => unreachable!("unhandled character class ({})", ch),
         }
     }
 
     fn handle_escape_sequence(&mut self, ch: char) -> Option<char> {
         match ch {
-            'f' => Some(unsafe { char::from_u32_unchecked(0xc) }),
+            'f' => Some(char::from_u32(0xc).expect("failed to convert code point to character")),
             'n' => Some('\n'),
             'r' => Some('\r'),
             't' => Some('\t'),
-            'v' => Some(unsafe { char::from_u32_unchecked(0xb) }),
+            'v' => Some(char::from_u32(0xb).expect("failed to convert code point to character")),
             '0' => Some('\0'),
             '^' | '$' | '\\' | '.' | '*' | '+' | '?' | '(' | ')' | '[' | ']' | '{' | '}' | '|'
             | '/' => Some(ch),
             'c' => {
-                // 'c' is followed by a letter from 'A'..='Z' or 'a'..='z'. The code point of the following letter modulo 32 is the code point of the escape sequence. This way control characters 1-26 can be used.
-                let Some(ch) = self.it.next() else {
+                // 'c' is followed by a letter from 'A'..='Z' or 'a'..='z'. The
+                // code point of the following letter modulo 32 is the code
+                // point of the escape sequence. This way control characters
+                // 1-26 can be used.
+
+                let Some((_, ch)) = self.it.next() else {
                     return None;
                 };
 
