ac119c17e4564fd8bdf6c05d71ca137259e00b6b
exellentcoin26 <jonas.caluwe26@gmail.com>
2023-08-12 18:28:53 +0200

    Refactor

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,29 @@
 use pango::Lexer;
 
 #[derive(Debug, Clone, Copy)]
-enum TokenKind {
+enum Foo {
     A,
     B,
     C,
     D { len: usize },
+    E,
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let lexer = Lexer::builder()
-        .with_token("aaaa", TokenKind::A)?
-        .with_token("b{4}", TokenKind::B)?
-        .with_token("b{4}", TokenKind::C)?
+        .with_token("aaaa", Foo::A)?
+        .with_token("b{4,}", Foo::B)?
+        .with_token("b{4}", Foo::C)?
         .with_token_map(
             "d*",
-            Box::new(|token_source| TokenKind::D {
+            Box::new(|token_source| Foo::D {
                 len: token_source.len(),
             }),
-        )?;
+        )?
+        .with_token(r"/\* .*", Foo::A)?
+        .with_token(r"/\* .* \*/", Foo::E)?;
 
-    for token in lexer.tokenize("bbbbbbbbaaaabbbbddddddddddd") {
+    for token in lexer.tokenize("bbbbbbbaaaabbbbddddddddddddddddd/* foo bar baz **** *") {
         println!("{:?}", token);
     }
 
